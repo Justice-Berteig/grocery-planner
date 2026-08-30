4c3b96db3cc598c56168df08f81d18c2b403667b
Justice Berteig <jberteig@gmail.com>
2026-08-30 17:17:54 -0400

    Added basic nested templates

--- main.go
+++ main.go
@@ -8,18 +8,34 @@ import (
 )
 
 
+type GroceryStore struct {
+	Name      string
+	Groceries []string
+}
+
+
 func main() {
 	fileServer := http.FileServer(http.Dir("./ui/static"))
 	http.Handle("GET /static/", http.StripPrefix("/static", fileServer))
 
-
-	http.HandleFunc(
-		"GET /{$}",
-		func(writer http.ResponseWriter, request *http.Request) {
-			indexTemplate := template.Must(template.ParseFiles("ui/html/index.html"))
-			indexTemplate.Execute(writer, nil)
-	})
+	http.HandleFunc( "GET /{$}", indexHandler)
 
 	fmt.Println("Starting server on port 8000...")
 	log.Fatal(http.ListenAndServe(":8000", nil))
+}
+
+
+func indexHandler(writer http.ResponseWriter, request *http.Request) {
+	data := map[string][]GroceryStore {
+		"GroceryStores": {
+			GroceryStore{ "Costco", []string{"Apple", "Banana"} },
+			GroceryStore{ "T&T"   , []string{"Cereal", "Bread"} },
+		},
+	}
+
+	tmpl := template.Must(template.ParseFiles(
+		"ui/html/index.html",
+		"ui/html/grocery-store.html",
+	))
+	tmpl.Execute(writer, data)
 }
